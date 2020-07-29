527d54ab423560f2779a8dadb570c644b4aca304
Erickson-Lopes-dev <ofc.erickson@gmail.com>
2020-07-29 16:13:08 -0300

    finished project

--- djangoum3/settings.py
+++ djangoum3/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangoum3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'index'
+
+LOGOUT_REDIRECT_URL = 'index'
